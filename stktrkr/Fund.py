74c6df32542e72761f633c93a7ba37dc287f7d64
Shane Brennan <brennash@Shanes-MacBook-Pro.local>
2016-02-01 20:19:24 +0000

    Cleaning up dataset

--- stktrkr/Fund.py
+++
@@ -1,8 +0,0 @@
-import re
-import csv
-import datetime
-from dateutil.relativedelta import relativedelta
-from stktrkr.Stock import Stock
-
-class Fund:
-	def __init__(self, fundName, verbose=False):
